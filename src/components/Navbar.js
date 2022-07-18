80b5ca0ed5f064bcfd4ace895c0dea46e7a490ce
Ilia Alekseev <iliaalekseevofb@gmail.com>
2022-07-18 22:53:47 +0500

    Navbar is a little bit improved

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,10 +4,8 @@ import { styled, alpha } from '@mui/material/styles';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
-import IconButton from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
 import InputBase from '@mui/material/InputBase';
-import MenuIcon from '@mui/icons-material/Menu';
 import SearchIcon from '@mui/icons-material/Search';
 import { createTheme, ThemeProvider } from '@mui/material';
 import { indigo } from '@mui/material/colors';
@@ -66,15 +64,6 @@ const Navbar = () => {
       <Box sx={{ flexGrow: 1, width: '100%'}}>
         <AppBar position="static">
           <Toolbar>
-            <IconButton
-                size="large"
-                edge="start"
-                color="inherit"
-                aria-label="open drawer"
-                sx={{ mr: 2 }}
-            >
-                <MenuIcon />
-            </IconButton>
             <Typography
                 variant="h6"
                 noWrap
