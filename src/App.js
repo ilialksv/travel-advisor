db09e3cac9d3ba968ab787cdbb52a41d62041232
Ilia Alekseev <iliaalekseevofb@gmail.com>
2022-07-18 22:48:24 +0500

    Navbar is partially done (not functional)

--- src/App.js
+++ src/App.js
@@ -3,17 +3,22 @@ import List from './components/List';
 import Map from './components/Map';
 import Navbar from './components/Navbar';
 import './App.css';
-import Container from '@mui/material/Container';
+import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid';
 
 const App = () => {
   return (
-    <Container maxWidth='xl'>
+    <Box>
       <Navbar />
       <Grid container spacing={3} sx={{width: '100%'}}>
-
+        <Grid item xs={12} md={4}>
+          <List />
+        </Grid>
+        <Grid item xs={12} md={8}>
+          <Map />
+        </Grid>
       </Grid>
-    </Container>
+    </Box>
   )
 }
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,105 @@
-import React from 'react'
+import React from 'react';
+import { Autocomplete } from '@react-google-maps/api';
+import { styled, alpha } from '@mui/material/styles';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import InputBase from '@mui/material/InputBase';
+import MenuIcon from '@mui/icons-material/Menu';
+import SearchIcon from '@mui/icons-material/Search';
+import { createTheme, ThemeProvider } from '@mui/material';
+import { indigo } from '@mui/material/colors';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: indigo[500]
+    }
+  }
+});
+
+const Search = styled('div')(({ theme }) => ({
+  position: 'relative',
+  borderRadius: theme.shape.borderRadius,
+  backgroundColor: alpha(theme.palette.common.white, 0.15),
+  '&:hover': {backgroundColor: alpha(theme.palette.common.white, 0.25)},
+  marginLeft: 0,
+  width: '100%',
+  [theme.breakpoints.up('sm')]: {
+    marginLeft: theme.spacing(1),
+    width: 'auto',
+  },
+}));
+
+const SearchIconWrapper = styled('div')(({ theme }) => ({
+  padding: theme.spacing(0, 2),
+  height: '100%',
+  position: 'absolute',
+  pointerEvents: 'none',
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+}));
+
+const StyledInputBase = styled(InputBase)(({ theme }) => ({
+  color: 'inherit',
+  '& .MuiInputBase-input': {
+    padding: theme.spacing(1, 1, 1, 0),
+    // vertical padding + font size from searchIcon
+    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+    transition: theme.transitions.create('width'),
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+      width: '12ch',
+      '&:focus': {
+        width: '20ch',
+      },
+    },
+  },
+}));
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
+    <ThemeProvider theme={theme}>
+      <Box sx={{ flexGrow: 1, width: '100%'}}>
+        <AppBar position="static">
+          <Toolbar>
+            <IconButton
+                size="large"
+                edge="start"
+                color="inherit"
+                aria-label="open drawer"
+                sx={{ mr: 2 }}
+            >
+                <MenuIcon />
+            </IconButton>
+            <Typography
+                variant="h6"
+                noWrap
+                component="div"
+                sx={{ flexGrow: 1, display: { xs: 'none', sm: 'block' } }}
+            >
+                Travel Advisor
+            </Typography>
+            <Typography sx={{mr: 3, display: {xs: 'none', md: 'block'}}}>
+                Explore new places
+            </Typography>
+            <Search>
+                <SearchIconWrapper>
+                <SearchIcon />
+                </SearchIconWrapper>
+                <StyledInputBase
+                placeholder="Search…"
+                inputProps={{ 'aria-label': 'search' }}
+                />
+            </Search>
+          </Toolbar>
+        </AppBar>
+      </Box>
+    </ThemeProvider>
+  );
 }
 
-export default Navbar
+export default Navbar;
