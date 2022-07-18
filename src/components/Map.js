1e62462263494aa775ac304f45bcb890a977e1f3
Ilia Alekseev <iliaalekseevofb@gmail.com>
2022-07-18 23:59:22 +0500

    Map component imports are done

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,5 +1,10 @@
 import React from 'react';
-
+import GoogleMapReact from 'google-map-react';
+import Paper from '@mui/material/Paper';
+import Typography from '@mui/material/Typography';
+import useMediaQuery from '@mui/material/useMediaQuery';
+import LocationOutlinedIcon from '@mui/icons-material/LocationOutlined';
+import Rating from '@mui/material/Rating';
 
 const Map = () => {
   return (
