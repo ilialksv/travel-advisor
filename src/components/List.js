6b498d67e3c07801538db425a9b4d9e6fd7f3cff
Ilia Alekseev <iliaalekseevofb@gmail.com>
2022-07-19 14:20:25 +0500

    Rating select is added to the List component

--- src/components/List.js
+++ src/components/List.js
@@ -12,17 +12,41 @@ import { createTheme, ThemeProvider } from '@mui/material'
 const List = () => {
   const theme = createTheme();
   const [type, setType] = useState('restaurants')
+  const [rating, setRating] = useState(0)
 
   return (
     <ThemeProvider theme={theme}>
       <Container sx={{padding: '25px'}}>
         <Typography variant='h4'>Restaurants, Hotels & Attractions around you</Typography>
-        <FormControl sx={{margin: theme.spacing(1)}}>
-          <InputLabel>Type</InputLabel>
-          <Select value={type} onChange={(e) => {setType(e.target.value)}}>
+        <FormControl 
+          variant="standard" 
+          sx={{margin: theme.spacing(1)}}
+        >
+          <InputLabel id="demo-simple-select-standard-label">Type</InputLabel>
+          <Select 
+            labelId="demo-simple-select-standard-label"
+            id="demo-simple-select-standard" 
+            value={type} 
+            onChange={(e) => {setType(e.target.value)}}
+          >
             <MenuItem value='restaurants'>Restaurants</MenuItem>
             <MenuItem value='hotels'>Hotels</MenuItem>
             <MenuItem value='attractions'>Attractions</MenuItem>
+          </Select>
+        </FormControl>
+        <FormControl variant="standard" sx={{margin: theme.spacing(1)}}>
+          <InputLabel id="demo-simple-select-standard-label">Rating</InputLabel>
+          <Select
+            labelId="demo-simple-select-standard-label"
+            label='Rating'
+            id="demo-simple-select-standard"
+            value={rating}
+            onChange={(e) => {setRating(e.target.value)}}
+          >
+            <MenuItem value={0}>All</MenuItem>
+            <MenuItem value={3}>Above 3.0</MenuItem>
+            <MenuItem value={4}>Above 4.0</MenuItem>
+            <MenuItem value={4.5}>Above 4.5</MenuItem>
           </Select>
         </FormControl>
       </Container>
