57614b8c21efc1da111aa2d9362ba357bcb3a379
Ilia Alekseev <iliaalekseevofb@gmail.com>
2022-07-19 13:41:32 +0500

    Google map is prepared and List necessary imports are made

--- src/components/List.js
+++ src/components/List.js
@@ -1,4 +1,11 @@
-import React from 'react'
+import React from 'react';
+import CircularProgress from '@mui/material/CircularProgress';
+import Grid from '@mui/material/Grid';
+import Typography from '@mui/material/Typography';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
 
 const List = () => {
   return (
