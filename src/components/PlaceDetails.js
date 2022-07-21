e693d79f23d1882b32459d904cfbd36dde757fd0
Ilia Alekseev <iliaalekseevofb@gmail.com>
2022-07-21 18:32:18 +0500

    Basic card layout is done

--- src/components/PlaceDetails.js
+++ src/components/PlaceDetails.js
@@ -1,8 +1,29 @@
-import React from 'react'
+import React from 'react';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import Card from '@mui/material/Card';
+import CardMedia from '@mui/material/CardMedia';
+import CardContent from '@mui/material/CardContent';
+import CardActions from '@mui/material/CardActions';
+import Chip from '@mui/material/Chip';
+import Rating from '@mui/material/Rating';
+import LocationOnIcon from '@mui/icons-material/LocationOn';
+import PhoneIcon from '@mui/icons-material/Phone';
 
-const PlaceDetails = ({place}) => {
+const PlaceDetails = ({ place }) => {
+  console.log(place);
   return (
-    <div>{place.name}</div>
+    <Card elecation={6}>
+      <CardMedia 
+        sx={{height: 350}}
+        image={place.photo ? place.photo.images.large.url : 'https://www.foodserviceandhospitality.com/wp-content/uploads/2016/09/Restaurant-Placeholder-001.jpg'}
+        title={place.name}
+      />
+      <CardContent>
+        <Typography gutterBottom variant='h5'>{ place.name }</Typography>
+      </CardContent>
+    </Card>
   )
 }
 
